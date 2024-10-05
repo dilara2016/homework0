6f90e4288b70c8b36095d83fefc994973b0147f7
dilara2016 <m.123.hunain@gmail.com>
2024-10-05 12:41:29 +0200

    homework2

---
+++ homework.py
@@ -0,0 +1,13 @@
+#Creating difrent varibles
+x = 8
+y = "dilara"
+print(x)
+print(y)
+
+codingal = "project "
+print(codingal)
+
+# User input'
+name = input("enter name:")
+
+print("\nHello",name, "\nwellcome to codingal")
